cae9e2bfaa8486a21f323cfa8fdb74618086d54a
rmaki <rabii.makk@gmail.com>
2022-04-13 19:35:42 -0300

    Se carga el Demo TAU bien piola ATR

--- src/test/java/taudemo/SeleniumTest.java
+++ src/test/java/taudemo/SeleniumTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 //todo
 @TestInstance(PER_CLASS)
 public class SeleniumTest {
-    private BrowserGetter browserGetter = new BrowserGetter();
+    private final BrowserGetter browserGetter = new BrowserGetter();
     private WebDriver driver;
 
     @BeforeAll
@@ -38,6 +38,7 @@ public class SeleniumTest {
         driver.get("https://www.example.com");
         assertEquals(expectedComTitle, driver.getTitle());
     }
+
     @Test
     public void openTheOrgPageAndCheckTheTitle() {
         String expectedOrgTitle = "Example Domain";
